d09a603f94113acb4fef4e8dfffe022ebd2fa189
Brian Kubisiak <brian@kubisiak.com>
2022-04-27 07:56:26 -0700

    home: add nonguix to skydio-desktop
    Nonguix isn't actually used anywhere, but having it available
    eliminates some errors in peregrine.scm when adding eyrie to the guix
    load path.
    
    Signed-off-by: Brian Kubisiak <brian@kubisiak.com>

--- eyrie/home/skydio-desktop.scm
+++ eyrie/home/skydio-desktop.scm
@@ -23,6 +23,16 @@
              git-dotfiles-service-type
              "brian.kubisiak@skydio.com")
             (service
+             guix-channels-service-type
+             '(list
+               (channel (name 'nonguix)
+                        (url "https://gitlab.com/nonguix/nonguix")
+                        (introduction
+                         (make-channel-introduction
+                          "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
+                          (openpgp-fingerprint
+                           "2A39 3FFF 68F4 EF7A 3D29 12AF 6F51 20A0 22FB B2D5"))))))
+            (service
              i3-dotfiles-service-type
              (i3-dotfiles-configuration
               (web-browser ungoogled-chromium)
