3c3016eea007b0c86f2f9aef0345abeb9333918d
Brian Kubisiak <brian@kubisiak.com>
2022-09-05 14:35:08 -0700

    home: server: Add new server profile
    This is intended for a more minimal headless environment. I plan on
    stripping this down further in the future to avoid any X11
    dependencies.
    
    Signed-off-by: Brian Kubisiak <brian@kubisiak.com>

---
+++ eyrie/home/server.scm
@@ -0,0 +1,38 @@
+;; SPDX-FileCopyrightText: 2022 Brian Kubisiak <brian@kubisiak.com>
+;;
+;; SPDX-License-Identifier: GPL-3.0-only
+
+(define-module (eyrie home server)
+  #:use-module (eyrie home dotfiles)
+  #:use-module (gnu home)
+  #:use-module (gnu home services shells)
+  #:use-module (gnu services)
+  #:use-module (guix gexp))
+
+(home-environment
+  (packages (append %devel-packages %cad-packages))
+  (services
+   (append (list
+            (service emacs-dotfiles-service-type)
+            (service
+             home-zsh-service-type
+             (home-zsh-configuration
+              (environment-variables %core-env)
+              (zshrc (zshrc-files #f))))
+            (service
+             git-dotfiles-service-type
+             "brian@kubisiak.com")
+            (service
+             guile-dotfiles-service-type
+             '((use-modules (ice-9 readline))
+               (activate-readline)))
+            (service
+             guix-channels-service-type
+             '(list
+               (channel (name 'nonguix)
+                        (url "https://gitlab.com/nonguix/nonguix")
+                        (introduction
+                         (make-channel-introduction
+                          "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
+                          (openpgp-fingerprint
+                           "2A39 3FFF 68F4 EF7A 3D29 12AF 6F51 20A0 22FB B2D5"))))))))))
