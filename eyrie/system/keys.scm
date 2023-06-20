7ce33f57197d805e2301463552e507a3c5718f7a
Brian Kubisiak <brian@kubisiak.com>
2023-06-19 20:35:02 -0700

    keys: Add SSH and signing keys for peregrine
    The SSH key is generally pretty useful to have; the signing key is
    required for using substitutes for condor.
    
    Signed-off-by: Brian Kubisiak <brian@kubisiak.com>

---
+++ eyrie/system/keys.scm
@@ -0,0 +1,12 @@
+;;; SPDX-FileCopyrightText: 2023 Brian Kubisiak <brian@kubisiak.com>
+;;;
+;;; SPDX-License-Identifier: GPL-3.0-only
+
+(define-module (eyrie system keys)
+  #:use-module (gnu))
+
+(define-public %peregrine-ssh-key
+  (local-file "peregrine-id-rsa.pub"))
+
+(define-public %peregrine-signing-key
+  (local-file "peregrine-signing-key.pub"))
