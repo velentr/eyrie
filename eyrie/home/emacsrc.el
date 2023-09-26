d5fb948a84a6f9642cfc6a5ecddc37a6bcca854e
Brian Kubisiak <brian@kubisiak.com>
2023-09-25 19:32:54 -0700

    home: emacs: Remove paredit in some mode
    Paredit is overall really nice, but unfortunately breaks
    'eval-expression' and the *scratch* buffer (probably due to an
    interaction with evil-mode). Only leave it enabled in scheme-mode for
    now.
    
    Signed-off-by: Brian Kubisiak <brian@kubisiak.com>

--- eyrie/home/emacsrc.el
+++ eyrie/home/emacsrc.el
@@ -171,11 +171,6 @@
 
 (autoload 'enable-paredit-mode "paredit"
   "Turn on pseudo-structural editing of Lisp code." t)
-(add-hook 'emacs-lisp-mode-hook       #'enable-paredit-mode)
-(add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
-(add-hook 'ielm-mode-hook             #'enable-paredit-mode)
-(add-hook 'lisp-mode-hook             #'enable-paredit-mode)
-(add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
 (add-hook 'scheme-mode-hook           #'enable-paredit-mode)
 
 (defun sane-lua-mode ()
