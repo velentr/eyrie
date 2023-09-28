velentr/eyrie

commit 72f82a8905c3b9440a8900b4ca6d1d0234a60f86

home: emacs: Add initial python tree-sitter support
This is mainly just an example of how to use the new support with
evil-mode. The keybindings allow (e.g.) 'vaf' and 'vif' to highlight a
function.

Signed-off-by: Brian Kubisiak <brian@kubisiak.com>

eyrie/home/emacsrc.el
@@ -196,6 +196,17 @@
 (add-hook 'rust-mode-hook 'eglot-ensure)
 (add-hook 'python-mode-hook 'eglot-ensure)
 
+;; treesit setup
+(require 'treesit)
+
+(setq major-mode-remap-alist
+ '((python-mode . python-ts-mode)))
+
+(define-key evil-outer-text-objects-map "f"
+            (evil-textobj-tree-sitter-get-textobj "function.outer"))
+(define-key evil-inner-text-objects-map "f"
+            (evil-textobj-tree-sitter-get-textobj "function.inner"))
+
 ;; emacs only defines 8 colors by default; define the other 8 using solarized
 ;; colors
 (tty-color-define "brightblack"    8 '(  0  43  54))

eyrie/home/dotfiles.scm
@@ -41,6 +41,7 @@
   #:use-module (gnu packages rust)
   #:use-module (gnu packages rust-apps)
   #:use-module (gnu packages terminals)
+  #:use-module (gnu packages tree-sitter)
   #:use-module (gnu packages valgrind)
   #:use-module (gnu packages version-control)
   #:use-module (gnu packages video)
@@ -138,6 +139,7 @@
    sshfs
    strace
    tree
+   tree-sitter-python
    usbutils
    valgrind
    vim
@@ -620,6 +622,7 @@ tztime local {
         emacs-eglot
         emacs-erlang
         emacs-evil
+        ey:emacs-evil-textobj-tree-sitter
         emacs-flycheck
         ey:emacs-github-mode
         emacs-go-mode

commit bc83693e48c0ba76fb56a9d85385d88261616d79

home: emacs: Fix eglot for python
We need to enable eglot for python-ts-mode since that's what we're
using.

Fixes: commit 72f82a8 ("home: emacs: Add initial python tree-sitter support")
Signed-off-by: Brian Kubisiak <brian@kubisiak.com>

eyrie/home/emacsrc.el
@@ -195,6 +195,7 @@
 (require 'eglot)
 (add-hook 'rust-mode-hook 'eglot-ensure)
 (add-hook 'python-mode-hook 'eglot-ensure)
+(add-hook 'python-ts-mode-hook 'eglot-ensure)
 
 ;; treesit setup
 (require 'treesit)