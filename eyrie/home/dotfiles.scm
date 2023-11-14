velentr/eyrie

commit a095216b75b1345d0563dfb505be17fc9fac0572

home: git: Add 'Fixes:' trailer section
To add a 'Fixes:' trailer to a commit message: 'git commit --trailer
fix=HEAD'. The trailer value is automatically formatted using 'git log
--oneline' to pretty-print the fixed commit.

Signed-off-by: Brian Kubisiak <brian@kubisiak.com>

eyrie/home/dotfiles.scm
@@ -211,6 +211,12 @@
            ("pull"       (("ff"         "only")))
            ("push"       (("default"    "simple")))
            ("rebase"     (("autosquash" "1")))
+           ("trailer \"fix\""
+            (("command"   "git log -1 --oneline --format='commit %h (\\\"%s\\\")' $ARG")
+             ("ifExists"  "addIfDifferent")
+             ("ifMissing" "add")
+             ("key"       "\"Fixes: \"")
+             ("where"     "start")))
            ("user"       (("name"       "Brian Kubisiak")
                           ("email"      ,email)))))
         (config-tree

commit 7456507d0c8dd87d968477a9ecbc885abdd15657

home: git: Fix 'Fixes:' trailers
I misunderstood how ifMissing and ifExists are implemented: the
command is first run without an argument to add the missing trailer,
then re-run with an argument to fix it. I need to replace the existing
trailer in order to get what I want.

This unfortunately means that I can't have multiple 'Fixes:' trailers,
but that should be fine.

Fixes: commit a095216 ("home: git: Add 'Fixes:' trailer section")
Signed-off-by: Brian Kubisiak <brian@kubisiak.com>

eyrie/home/dotfiles.scm
@@ -213,7 +213,7 @@
            ("rebase"     (("autosquash" "1")))
            ("trailer \"fix\""
             (("command"   "git log -1 --oneline --format='commit %h (\\\"%s\\\")' $ARG")
-             ("ifExists"  "addIfDifferent")
+             ("ifExists"  "replace")
              ("ifMissing" "add")
              ("key"       "\"Fixes: \"")
              ("where"     "start")))