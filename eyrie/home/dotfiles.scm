velentr/eyrie

commit a095216b75b1345d0563dfb505be17fc9fac0572

home: git: Add 'Fixes:' trailer section
To add a 'Fixes:' trailer to a commit message: 'git commit --trailer
fix=HEAD'. The trailer value is automatically formatted using 'git log
--oneline' to pretty-print the fixed commit.

Signed-off-by: Brian Kubisiak <brian@kubisiak.com>

eyrie/home/dotfiles.scm
@@ -211,6 +211,12 @@
            ("pull"       (("ff"         "only")))
            ("push"       (("default"    "simple")))
            ("rebase"     (("autosquash" "1")))
+           ("trailer \"fix\""
+            (("command"   "git log -1 --oneline --format='commit %h (\\\"%s\\\")' $ARG")
+             ("ifExists"  "addIfDifferent")
+             ("ifMissing" "add")
+             ("key"       "\"Fixes: \"")
+             ("where"     "start")))
            ("user"       (("name"       "Brian Kubisiak")
                           ("email"      ,email)))))
         (config-tree

commit eea3d92ddff8827cf67d8bb6122fdb7fade0e877

home: git: Fix 'Fixes:' trailers again
Upstream kernel does not specify 'commit' as part of the 'Fixes:'
trailer (and checkpatch.pl warns about this). Remove this to conform
to upstream.

Fixes: a095216 ("home: git: Add 'Fixes:' trailer section")
Signed-off-by: Brian Kubisiak <brian@kubisiak.com>

eyrie/home/dotfiles.scm
@@ -207,7 +207,7 @@
            ("push"       (("default"    "simple")))
            ("rebase"     (("autosquash" "1")))
            ("trailer \"fix\""
-            (("command"   "git log -1 --oneline --format='commit %h (\\\"%s\\\")' $ARG")
+            (("command"   "git log -1 --oneline --format='%h (\\\"%s\\\")' $ARG")
              ("ifExists"  "replace")
              ("ifMissing" "add")
              ("key"       "\"Fixes: \"")