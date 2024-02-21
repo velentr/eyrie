430acfa369d2343c00c8e5b44122ad7dd88f606b
Brian Kubisiak <brian@kubisiak.com>
2024-02-21 04:52:11 -0800

    home: rsnapshot: Initial revision
    This includes a bunch of hardcoded config, but is a good starting
    point. Start by backing up radicale data on e3r3.
    
    Signed-off-by: Brian Kubisiak <brian@kubisiak.com>

---
+++ eyrie/home/rsnapshot.scm
@@ -0,0 +1,63 @@
+;;; SPDX-FileCopyrightText: 2024 Brian Kubisiak <brian@kubisiak.com>
+;;;
+;;; SPDX-License-Identifier: GPL-3.0-only
+
+(define-module (eyrie home rsnapshot)
+  #:use-module (gnu home services mcron)
+  #:use-module (gnu packages backup)
+  #:use-module (gnu packages base)
+  #:use-module (gnu packages rsync)
+  #:use-module (gnu packages ssh)
+  #:use-module (gnu services)
+  #:use-module (gnu services configuration)
+  #:use-module (guix gexp)
+  #:use-module (guix records)
+  #:export (home-rsnapshot-service-type
+            home-rsnapshot-configuration))
+
+(define-configuration/no-serialization home-rsnapshot-configuration
+  (rsnapshot (file-like rsnapshot) "The rsnapshot package to use.")
+  (rsync (file-like rsync) "The rsync package to use.")
+  (coreutils (file-like coreutils) "The coreutils package to use.")
+  (ssh (file-like openssh-sans-x) "The SSH package to use.")
+  (directory (string (string-append (getenv "HOME") "/bak"))
+             "Local directory to store backed-up files."))
+
+(define (rsnapshot-configuration config)
+  (match-record config <home-rsnapshot-configuration>
+                (coreutils directory rsnapshot rsync ssh)
+    (mixed-text-file
+     "rsnapshot.conf"
+     "config_version\t1.2\n"
+     "snapshot_root\t" directory "\n"
+     "cmd_cp\t" coreutils "/bin/cp\n"
+     "cmd_rm\t" coreutils "/bin/rm\n"
+     "cmd_rsync\t" rsync "/bin/rsync\n"
+     "cmd_ssh\t" ssh "/bin/ssh\n"
+     "cmd_du\t" coreutils "/bin/du\n"
+     "cmd_rsnapshot_diff\t" rsnapshot "/bin/rsnapshot-diff\n"
+
+     "retain\talpha\t7\n"
+
+     "verbose\t2\n"
+     "loglevel\t3\n"
+     "lockfile\t/run/user/1000/rsnapshot.pid\n"
+     "backup\troot@e3r3.com:/var/lib/radicale/\te3r3/\n")))
+
+(define (home-rsnapshot-configuration-jobs config)
+  (match-record config <home-rsnapshot-configuration>
+                (rsnapshot)
+    (list #~(job '(next-hour '(0))  ; daily at midnight
+                 (string-append #$rsnapshot "/bin/rsnapshot -c "
+                                #$(rsnapshot-configuration config)
+                                " alpha")))))
+
+(define home-rsnapshot-service-type
+  (service-type
+   (name 'home-rsnapshot)
+   (extensions
+    (list (service-extension home-mcron-service-type
+                             home-rsnapshot-configuration-jobs)))
+   (default-value (home-rsnapshot-configuration))
+   (description
+    "Configure backups using rsnapshot.")))
