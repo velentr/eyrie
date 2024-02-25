3d717f5149b1bf2e13f03f20b5da1f915cd51d02
Brian Kubisiak <brian@kubisiak.com>
2024-02-25 09:04:38 -0800

    home: rsnapshot: Configure backup levels
    Instead of hardcoding these, allow the user to configure these. This
    centralizes the source-of-truth for both the cron jobs and the
    'retain' settings in the config file, making it easier to modify the
    jobs that run.
    
    Additionally, add back the beta and gamma jobs to run weekly and
    monthly backups, respectively.
    
    Signed-off-by: Brian Kubisiak <brian@kubisiak.com>

--- eyrie/home/rsnapshot.scm
+++ eyrie/home/rsnapshot.scm
@@ -12,8 +12,30 @@
   #:use-module (gnu services configuration)
   #:use-module (guix gexp)
   #:use-module (guix records)
+  #:use-module (srfi srfi-26)
   #:export (home-rsnapshot-service-type
             home-rsnapshot-configuration))
+
+(define (list-or-string? val)
+  (or (list? val)
+      (string? val)))
+
+(define-configuration/no-serialization rsnapshot-backup-level
+  (name string "Name of the backup level.")
+  (retention integer "Number of backups to keep for this interval.")
+  (interval list-or-string "An mcron time specification indicating how often to
+back up this level."))
+
+(define (rsnapshot-backup-level->retain-configuration level)
+  (match-record level <rsnapshot-backup-level>
+                (name retention)
+    (format #f "retain\t~a\t~d\n" name retention)))
+
+(define (rsnapshot-backup-level->mcron-job level make-rsnapshot-command)
+  (match-record level <rsnapshot-backup-level>
+                (name interval)
+    #~(job #$interval
+           #$(make-rsnapshot-command name))))
 
 (define-configuration/no-serialization home-rsnapshot-configuration
   (rsnapshot (file-like rsnapshot) "The rsnapshot package to use.")
@@ -21,7 +43,23 @@
   (ssh (file-like openssh-sans-x) "The SSH package to use.")
   (directory (string (string-append (getenv "HOME") "/bak"))
              "Local directory to store backed-up files.")
-  (backups (list '()) "List of backups to make."))
+  (backups (list '()) "List of backups to make.")
+  (backup-levels (list
+                  (list
+                   (rsnapshot-backup-level
+                    (name "alpha")
+                    (retention 6)
+                    (interval ''(next-hour '(0))))  ; daily at midnight
+                   (rsnapshot-backup-level
+                    (name "beta")
+                    (retention 4)
+                    (interval "15 0 * * 1"))  ; Monday 12:15 AM
+                   (rsnapshot-backup-level
+                    (name "gamma")
+                    (retention 6)
+                    ;; 12:30 AM on the first of every month
+                    (interval ''(next-minute-from (next-day '(1)) '(30))))))
+                 "List of backup levels to keep."))
 
 (define (rsnapshot-configuration config)
   (define (make-backup-settings backups)
@@ -31,7 +69,7 @@
             (list "backup\t" (car backup) "\t" (cadr backup) "\n"))
           backups)))
   (match-record config <home-rsnapshot-configuration>
-                (backups directory rsnapshot rsync ssh)
+                (backups backup-levels directory rsnapshot rsync ssh)
     (let ((cmds
            (cons*
             "cmd_cp\t" coreutils "/bin/cp\n"
@@ -53,16 +91,18 @@
          "lockfile\t/run/user/" (number->string (getuid)) "/rsnapshot.pid\n")
         cmds
 
-        (list "retain\talpha\t7\n")
+        (map rsnapshot-backup-level->retain-configuration backup-levels)
         backups)))))
 
 (define (home-rsnapshot-configuration-jobs config)
   (match-record config <home-rsnapshot-configuration>
-                (rsnapshot)
-    (list #~(job '(next-hour '(0))  ; daily at midnight
-                 (string-append #$rsnapshot "/bin/rsnapshot -c "
-                                #$(rsnapshot-configuration config)
-                                " alpha")))))
+                (rsnapshot backup-levels)
+    (define (make-rsnapshot-command level-name)
+      #~(string-append #$rsnapshot "/bin/rsnapshot -c "
+                       #$(rsnapshot-configuration config)
+                       " " #$level-name))
+    (map (cut rsnapshot-backup-level->mcron-job <> make-rsnapshot-command)
+         backup-levels)))
 
 (define home-rsnapshot-service-type
   (service-type
