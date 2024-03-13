velentr/eyrie

commit e91dba62cb56bfbd108101479535f07164ce1462

home: skydio-desktop: Run a nightly 'nix store optimise'
Unlike guix, nix requires regular calls to deduplicate files in the
store with hardlinks. Add this every night at midnight.

Signed-off-by: Brian Kubisiak <brian@kubisiak.com>

eyrie/home/skydio-desktop.scm
@@ -5,6 +5,7 @@
 (define-module (eyrie home skydio-desktop)
   #:use-module (eyrie home dotfiles)
   #:use-module (gnu home)
+  #:use-module (gnu home services mcron)
   #:use-module (gnu home services shells)
   #:use-module (gnu packages chromium)
   #:use-module (gnu packages emacs-xyz)
@@ -22,6 +23,11 @@
               (plugins (cons* emacs-dockerfile-mode
                               emacs-protobuf-mode
                               %core-emacs-plugins))))
+            (service
+             home-mcron-service-type
+             (home-mcron-configuration
+              (jobs (list #~(job '(next-hour '(0))
+                                 "nix store optimise")))))
             (service
              home-zsh-service-type
              (home-zsh-configuration

commit aeba84ded77211ba9da1ee514fd96da634b8060f

home: skydio-desktop: Fix nightly 'nix store optimise'
The mcron job doesn't have $PATH set the same as a login session
(should have expected this). Instead of relying on $PATH, point at nix
in the store.

Fixes: e91dba6 ("home: skydio-desktop: Run a nightly 'nix store optimise'")
Signed-off-by: Brian Kubisiak <brian@kubisiak.com>

eyrie/home/skydio-desktop.scm
@@ -9,6 +9,7 @@
   #:use-module (gnu home services shells)
   #:use-module (gnu packages chromium)
   #:use-module (gnu packages emacs-xyz)
+  #:use-module (gnu packages package-management)
   #:use-module (gnu packages protobuf)
   #:use-module (gnu services)
   #:use-module (guix gexp))
@@ -27,7 +28,9 @@
              home-mcron-service-type
              (home-mcron-configuration
               (jobs (list #~(job '(next-hour '(0))
-                                 "nix store optimise")))))
+                                 #$(file-append
+                                    nix
+                                    "/bin/nix store optimise"))))))
             (service
              home-zsh-service-type
              (home-zsh-configuration