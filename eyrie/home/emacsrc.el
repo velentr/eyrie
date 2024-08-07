velentr/eyrie

commit f5a605417d66820396e39610ed9ca7be8b4dc717

home: emacs: Configure formatting in aircam
In all aircam[0-9] workspaces, add the python venv to the exec-path so
format-all-the-code can find ruff. Also set the default python
formatter to ruff instead of black, since black will format some
things incorrectly.

Signed-off-by: Brian Kubisiak <brian@kubisiak.com>

eyrie/home/emacsrc.el
@@ -21,9 +21,20 @@
 
 (add-hook 'find-file-hook
           #'(lambda ()
-             (let ((proj (project-current)))
-               (if proj
-                   (setq default-directory (project-root proj))))))
+              (let ((proj (project-current)))
+                (if proj
+                    (setq default-directory (project-root proj)))
+                (if (string-match "~/aircam[0-9]*" default-directory)
+                    (progn
+                      (setq exec-path
+                            (append
+                             exec-path
+                             (list
+                              (concat
+                               default-directory
+                               "/build/python3_venv/bin"))))
+                      (setq format-all-formatters
+                            '(("Python" (ruff "format")))))))))
 
 (setq-default indent-tabs-mode nil)
 
@@ -261,15 +272,6 @@
 ;; scan through flycheck errors
 (define-key leader-map "[" (kbd "C-c ! p"))
 (define-key leader-map "]" (kbd "C-c ! n"))
-
-;; run code_format
-(define-key leader-map "f"
-  (lambda ()
-    (interactive)
-    (save-buffer)
-    (call-process
-     "~/aircam/skyrun" nil nil nil "bin" "code_format" buffer-file-name)
-    (revert-buffer t t t)))
 
 ;; search for a file in the git repo
 (define-key leader-map "s" #'project-find-file)

commit 930d37489015b24080faca4fce0604bc95c9beae

home: emacs: Fix C++ formatting in aircam
When loading aircam code, we want to set the python default formatter,
not overwrite all existing formatters.

Fixes: f5a6054 ("home: emacs: Configure formatting in aircam")
Signed-off-by: Brian Kubisiak <brian@kubisiak.com>

eyrie/home/emacsrc.el
@@ -34,7 +34,8 @@
                                default-directory
                                "/build/python3_venv/bin"))))
                       (setq format-all-formatters
-                            '(("Python" (ruff "format")))))))))
+                            (cons '("Python" (ruff "format"))
+                                  format-all-formatters)))))))
 
 (setq-default indent-tabs-mode nil)
 