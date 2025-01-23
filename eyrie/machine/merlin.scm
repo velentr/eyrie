56b0b5cbdebfe7e06c0b746296fc003ba2fd986b
Brian Kubisiak <brian@kubisiak.com>
2025-01-22 16:34:53 -0800

    machine: merlin: Use microguix kernel and rauc
    Since I have the microguix channel installed, pull the kernel from
    there. Also add rauc to the package list since I'll need it installed.
    
    Signed-off-by: Brian Kubisiak <brian@kubisiak.com>

--- eyrie/machine/merlin.scm
+++ eyrie/machine/merlin.scm
@@ -20,33 +20,14 @@
   #:use-module (guix git-download)
   #:use-module (guix packages)
   #:use-module (guix platforms arm)
+  #:use-module (micrognu packages updates)
+  #:use-module (micrognu packages vendor globalscaletechnologies)
   #:use-module (nongnu packages linux)
   #:use-module (eyrie system keys))
 
-(define linux-espressobin-ultra
-  (package
-    (inherit
-     (customize-linux
-      #:name "linux-espressobin-ultra"
-      #:source (origin
-                 (method git-fetch)
-                 (uri (git-reference
-                       (url "https://github.com/globalscaletechnologies/linux.git")
-                       (commit "linux-5.4.53-gti")))
-                 (file-name (git-file-name "linux-espressobin-ultra" "5.4.53-gti"))
-                 (sha256
-                  (base32 "1pwpn6wrz6ydx62gp9g2drapg126lwihcr0yhhcqilc1cxy7m02q")))
-      #:defconfig (local-file "gti_ccpe-88f3720_defconfig")
-      #:extra-version "guix"))
-    (version "5.4.53-gti")
-    (home-page "https://github.com/globalscaletechnologies/linux")
-    (synopsis "Linux kernel with patches from globalscale technologies")
-    (description "Linux kernel build from globalscale technologies sources,
-including nonfree binary blobs.")))
-
 (define espressobin-ultra-barebones-os
   (operating-system
-    (kernel linux-espressobin-ultra)
+    (kernel linux-espressobin-ultra-5.4)
     (kernel-arguments
      (cons "console=ttyMV0,115200"
            %default-kernel-arguments))
@@ -85,6 +66,9 @@ including nonfree binary blobs.")))
  (operating-system
   (operating-system
     (inherit espressobin-ultra-barebones-os)
+    (packages
+     (cons* rauc
+            %base-packages))
     (services
      (cons* (service dhcp-client-service-type
                      (dhcp-client-configuration
