99d1cb9dc6795f6bebabf5260494160e12780863
Brian Kubisiak <brian@kubisiak.com>
2025-02-17 11:29:55 -0800

    home: zsh: Fix git autocomplete
    A package update for git broke autocomplete for zsh; work around this
    in .zshrc until I upsteam a more permanent fix.
    
    Signed-off-by: Brian Kubisiak <brian@kubisiak.com>

--- eyrie/home/autocomplete.zsh
+++ eyrie/home/autocomplete.zsh
@@ -11,6 +11,8 @@ setopt completealiases
 eval `dircolors -b`
 zstyle ':completion:*' list-colors '${(s.:.)LS_COLORS}'
 zstyle ':completion:*' menu select eval "$(dircolors -b)"
+# guix's completion for git is broken
+zstyle ':completion:*:*:git:*' script ~/.guix-home/profile/etc/bash_completion.d/git
 
 if [ -f "${HOME}/aircam/build/python3_venv/bin/register-python-argcomplete" ]; then
 	eval "$(${HOME}/aircam/build/python3_venv/bin/register-python-argcomplete --shell zsh skyrun)"
